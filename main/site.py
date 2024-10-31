cd0207c7712e10905a9fbe1434acb7baeb65f663
Robert Shonk <rshonkcode@gmail.com>
2024-10-30 18:49:34 -0700

    css changes and routed stats page.

--- main/site.py
+++ main/site.py
@@ -12,3 +12,32 @@ bp = Blueprint('site', __name__)
 @bp.route('/')
 def index():
     return render_template('site/index.html')
+
+
+@bp.route('/stats', methods=('GET', 'POST'))
+def stats():
+    if request.method == 'POST':
+        # do something if post (probably want to update info)
+        return 'stats post'
+
+    # get
+    summoner_name = request.args.get('summoner_name')
+    tagline = request.args.get('tagline')
+
+    # check db if summoner exists
+    db = get_db()
+    data = db.execute(
+        """
+        SELECT * FROM league WHERE summoner_name = ? AND tagline = ?
+        """,
+        (summoner_name, tagline)
+    ).fetchone()
+
+    # if exists, render template with player info
+    if data:
+        return render_template('site/stats.html', data=data)
+
+    
+    # if not render with none
+
+    return render_template('site/stats.html', summoner_name=summoner_name, tagline=tagline)
